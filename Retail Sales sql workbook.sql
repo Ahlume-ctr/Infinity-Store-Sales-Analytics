15df7f10a5f0a53be7352941fcdefde7e4c0f3c4
Ahlume-ctr <ahlumengcingane@gmail.com>
2026-05-11 22:20:30 +0000

    Cleaning the retail sales sql workbook

--- Retail Sales sql workbook.sql
+++ Retail Sales sql workbook.sql
@@ -1,3 +1,10 @@
 -- Databricks notebook source
-SELECT *
-FROM workspace.default.retail_store_sales
+SELECT TRY_CAST(GET(SPLIT(transactionId, "_"), 1) AS INT) AS TransactionID,
+         TRY_CAST(GET(SPLIT(customerId, "_"), 1) AS INT) AS CustomerID,
+           category AS Categoty,
+             TRY_CAST(GET(SPLIT(item, "_"), 1) AS INT) AS Item,
+                quantity AS Quantity,
+                  totalSpent AS Total_Spent,
+                     discountApplied AS Discount_Applied
+FROM workspace.default.retail_store_sales;
+
